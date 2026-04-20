bd350b19c11516b931f55fa224fdba331732b50b
fabioscarda <fabio.scarda@gmail.com>
2026-04-20 11:46:56 +0200

    toolbar

--- V2.0/draw_app/src/components/Toolbar.tsx
+++ V2.0/draw_app/src/components/Toolbar.tsx
@@ -36,6 +36,14 @@ function Toolbar({ setColor, setSize, setTool, tool, color }: ToolbarProps) {
 
   return (
     <div>
+      <span className="" onClick={}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z"/></svg>
+      </span>
+
+      <span className="" onClick={}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M396-200q-97 0-166.5-63T160-420q0-94 69.5-157T396-640h252L544-744l56-56 200 200-200 200-56-56 104-104H396q-63 0-109.5 40T240-420q0 60 46.5 100T396-280h284v80H396Z"/></svg>
+      </span>
+
       <input
         type="color"
         onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
